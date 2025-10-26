c83dea8fecd853906b7f225fe0beef95dd68448d
Overgrown <overgrownmc@gmail.com>
2025-10-26 13:12:43 -0400

    Reset aspect and Aura Node visibility

--- src/main/java/dev/overgrown/aspectslib/AspectsLibClient.java
+++ src/main/java/dev/overgrown/aspectslib/AspectsLibClient.java
@@ -43,11 +43,11 @@ public class AspectsLibClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // Initialize default tooltip visibility to shown
-        AspectsTooltipConfig.setAlwaysShow(true);
+        AspectsTooltipConfig.setAlwaysShow(false);
 
         // Initialize aura node visibility to hidden by default
         // No conditions are set by default - other mods must add them via API
-        AuraNodeVisibilityConfig.setAlwaysShow(true);
+        AuraNodeVisibilityConfig.setAlwaysShow(false);
 
         EntityRendererRegistry.register(ModEntities.AURA_NODE, AuraNodeRenderer::new);
 
